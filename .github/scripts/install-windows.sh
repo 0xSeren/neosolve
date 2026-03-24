ae30f03db14c744b79204110d07cab79fc44c6c6
0xSeren <0xseren@tutamail.com>
2026-03-24 18:40:39 -0400

    Fix Windows CI vcpkg path

--- .github/scripts/install-windows.sh
+++ .github/scripts/install-windows.sh
@@ -1,8 +1,16 @@
 #!/bin/sh -xe
 
-# Install OpenCASCADE via vcpkg
-git clone https://github.com/microsoft/vcpkg.git /c/vcpkg
-/c/vcpkg/bootstrap-vcpkg.bat
-/c/vcpkg/vcpkg install opencascade:x64-windows opencascade:x86-windows
+# Install OpenCASCADE via vcpkg (vcpkg is pre-installed on GitHub Actions)
+# Use VCPKG_ROOT if set, otherwise default to C:/vcpkg
+VCPKG_DIR="${VCPKG_INSTALLATION_ROOT:-/c/vcpkg}"
 
+# Update vcpkg to latest
+cd "$VCPKG_DIR"
+git pull || true
+./bootstrap-vcpkg.bat
+
+# Install OpenCASCADE for both platforms
+./vcpkg install opencascade:x64-windows opencascade:x86-windows --clean-after-build
+
+cd -
 git submodule update --init
