6152835bbb4efe04834063bf5bd5482bedb63ab5
0xSeren <0xseren@tutamail.com>
2026-04-09 19:26:48 -0400

    Fix Linux build (OCC 7.6 compat, add TBB dep) and remove redundant CI tests

--- .github/scripts/install-ubuntu.sh
+++ .github/scripts/install-ubuntu.sh
@@ -6,6 +6,6 @@ sudo apt-get install -q -y \
   zlib1g-dev libpng-dev libcairo2-dev libfreetype6-dev libjson-c-dev \
   libfontconfig1-dev libgtkmm-3.0-dev libpangomm-1.4-dev libgl-dev \
   libgl-dev libglu-dev libspnav-dev \
-  libocct-modeling-algorithms-dev libocct-data-exchange-dev
+  libocct-modeling-algorithms-dev libocct-data-exchange-dev libtbb-dev
 
 git submodule update --init extlib/libdxfrw extlib/mimalloc extlib/eigen
